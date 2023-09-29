e8b9c8934256c080d3c979722500372f74dac8c5
Vc <vcvckw@gmail.com>
2023-09-30 01:34:56 +0800

    init

--- main.py
+++ main.py
@@ -12,24 +12,28 @@ def run():
     t2 = threading.Thread(target=oda.get_news, args=())
     t3 = threading.Thread(target=Tl.get_news, args=())
     # t4 = threading.Thread(target=coin_time.get_news, args=())
+    t4 = threading.Thread(target=server, args=())
     t1.start()
     time.sleep(3)
     t2.start()
     time.sleep(3)
     t3.start()
+    t4.start()
     # t4.start()
 
-
-if __name__ == '__main__':
+def server():
     webdir = './index'  # 设置网站的根目录为程序所在路径
     port = 80  # 设置一个端口
     os.chdir(webdir)
     server_address = ('', port)  # 设置服务器地址
     server_obj = HTTPServer(server_address, CGIHTTPRequestHandler)  # 创建服务器对象
-    # run()
-    Js.get_news()
+    server_obj.serve_forever()  # 启动服务器
+
+if __name__ == '__main__':
+
+    run()
+    # Js.get_news()
     # oda.get_news()
     # ti.get_news()
     # Tl.get_news()
     # coin_time.get_news()
-    server_obj.serve_forever()  # 启动服务器
