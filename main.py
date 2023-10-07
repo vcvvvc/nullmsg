d4a599a7c20bf2be54f1ad2780688ae2be4d2bfb
Vc <vcvckw@gmail.com>
2023-10-07 19:11:00 +0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import threading
 import os
 import time
+import requests
 
 from http.server import HTTPServer, CGIHTTPRequestHandler
 from src.jinse import Js
@@ -12,13 +13,11 @@ def run():
     t2 = threading.Thread(target=oda.get_news, args=())
     t3 = threading.Thread(target=Tl.get_news, args=())
     t4 = threading.Thread(target=server, args=())
-    t5 = threading.Thread(target=Js.heartbeat, args=())
     t1.start()
     time.sleep(3)
     t2.start()
     time.sleep(3)
     t3.start()
-    t5.start()
     t4.start()
 
 def server():
@@ -29,8 +28,25 @@ def server():
     server_obj = HTTPServer(server_address, CGIHTTPRequestHandler)  # 创建服务器对象
     server_obj.serve_forever()  # 启动服务器
 
+def heartbeat(): #render无活动时间久了会暂停服务，定时get活动一下
+    while True:
+        time.sleep(40)
+        pushurl = 'https://pushmsg.onrender.com'
+        sendurl = 'https://bark-test-cje9.onrender.com'
+        headers2 = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'
+        }
+        res = requests.get(sendurl, headers=headers2, timeout=30)
+        if res.status_code != 200:
+            res = requests.get(sendurl, headers=headers2, timeout=30)
+
+        requests.get(pushurl, headers=headers2, timeout=30)
+        if res.status_code != 200:
+            requests.get(pushurl, headers=headers2, timeout=30)
+
 if __name__ == '__main__':
     run()
+    heartbeat()
     # Js.get_news()
     # oda.get_news()
     # ti.get_news()
