21af6723eda12b06aa71b56d5e8e97a8b2b4cc0d
Vc <vcvckw@gmail.com>
2023-10-10 15:36:20 +0800

    init

--- main.py
+++ main.py
@@ -31,18 +31,21 @@ def server():
 def heartbeat(): #render无活动时间久了会暂停服务，定时get活动一下
     while True:
         time.sleep(40)
-        pushurl = 'https://pushmsg.onrender.com'
-        sendurl = 'https://bark-test-cje9.onrender.com'
-        headers2 = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'
-        }
-        res = requests.get(sendurl, headers=headers2, timeout=30)
-        if res.status_code != 200 or res.status_code != 404:
+        try:
+            pushurl = 'https://pushmsg.onrender.com'
+            sendurl = 'https://bark-test-cje9.onrender.com'
+            headers2 = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'
+            }
             res = requests.get(sendurl, headers=headers2, timeout=30)
+            if res.status_code != 200 or res.status_code != 404:
+                res = requests.get(sendurl, headers=headers2, timeout=30)
 
-        requests.get(pushurl, headers=headers2, timeout=30)
-        if res.status_code != 200 or res.status_code != 404:
             requests.get(pushurl, headers=headers2, timeout=30)
+            if res.status_code != 200 or res.status_code != 404:
+                requests.get(pushurl, headers=headers2, timeout=30)
+        except:
+            continue
 
 if __name__ == '__main__':
     run()
